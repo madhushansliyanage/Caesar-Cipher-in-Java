35e1743a3bc648fa1babc34d805a0974a8cea40b
madhushansliyanage <madhushansliyanage@gmail.com>
2024-12-08 11:26:57 +0530

    Add decryption algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -7,43 +8,83 @@ public class Main {
     //Array 'alphabetC' Store the Uppercase letters of English alphabet
     private static char[] sAlphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-    private static char getEncryptedLetter(char character) {
-        //Iterate through the alphabet to find characters are uppercase or lowercase
-        char encryptedLetter = character;
-        for (int j = 0; j < 26; j++) {
-            //check whether each character is uppercase
-            if (character==cAlphabet[j]) {
-                encryptedLetter = cAlphabet[((j + 3) % 26)];
-                break;
-            } else if (character == sAlphabet[j]) {
-                encryptedLetter = sAlphabet[((j + 3) % 26)];
-                break;
+    private static String getEncryptedMessage(String message) {
+        StringBuilder encryptedText = new StringBuilder();
+
+        for (int i = 0; i < message.length(); i++) {
+            char encryptedLetter = message.charAt(i);
+            //Iterate through the alphabet to find characters are uppercase or lowercase
+            for (int j = 0; j < 26; j++) {
+                //check character is uppercase
+                if (message.charAt(i) == cAlphabet[j]) {
+                    encryptedLetter = cAlphabet[((j + 3) % 26)];
+                    break;
+                    //check character is lowercase
+                } else if (message.charAt(i) == sAlphabet[j]) {
+                    encryptedLetter = sAlphabet[((j + 3) % 26)];
+                    break;
+                }
             }
+            encryptedText.append(encryptedLetter);
         }
-        return encryptedLetter;
+
+        return encryptedText.toString();
+    }
+
+    private static String getDecryptedLetter(String encryptedMessage) {
+        StringBuilder decryptedText = new StringBuilder();
+
+        for (int i = 0; i < encryptedMessage.length(); i++) {
+            char decryptedLetter = encryptedMessage.charAt(i);
+            for (int j = 0; j < 26; j++) {
+                if (encryptedMessage.charAt(i) == cAlphabet[j]) {
+                    decryptedLetter = cAlphabet[(j - 3 + 26) % 26];
+                    break;
+                } else if (encryptedMessage.charAt(i) == sAlphabet[j]) {
+                    decryptedLetter = sAlphabet[((j - 3 + 26) % 26)];
+                    break;
+                }
+            }
+            decryptedText.append(decryptedLetter);
+        }
+        return decryptedText.toString();
     }
 
     public static void main(String[] args) {
         System.out.println("-------------------Caeser Cipher Encryption Algorithm------------------------");
-
+        Scanner input = new Scanner(System.in);
         String encryptedMessage = "";
 
-        System.out.print("Enter a message:");
-        Scanner input = new Scanner(System.in);
-        String message = input.nextLine();
+        System.out.print("SELECT OPTION\n1.ENCRYPT\n2.DECRYPT :");
+        try {
+            int option = input.nextInt();
+            input.nextLine();
 
-        if (!message.isEmpty()) {
-            //Going through each character in the message
-            for (int i = 0; i < message.length(); i++) {
-                String encryptrdLetter = String.valueOf(getEncryptedLetter(message.charAt(i)));
-                encryptedMessage=encryptedMessage.concat(encryptrdLetter);
+
+            switch (option) {
+                case 1:
+                    System.out.print("Enter a message:");
+                    String message = input.nextLine();
+
+                    System.out.println("Encrypted Message: " + getEncryptedMessage(message));
+
+                    break;
+                case 2:
+                    System.out.print("Enter decrypted message:");
+                    String cipherText = input.nextLine();
+
+                    System.out.println("Decripted Message: " + getDecryptedLetter(cipherText));
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid Option");
             }
 
-            System.out.println("Encrypted Message: " + encryptedMessage);
-        } else {
-            System.out.println("Your message is empty!");
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please try again.");
         }
-
     }
 
 }
